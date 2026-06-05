69a0029a149920fd5a0868c2c19ad8aac6f661c1
shenjh <skyer_tech@163.com>
2026-06-05 17:50:32 +0800

    add 初始提交

---
+++ src/main/java/com/lulala/langchain4j/LangChain4jDemoApplication.java
@@ -0,0 +1,19 @@
+package com.lulala.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author shenjh
+ * @since 2026/6/5 17:49
+ * @version 1.0
+ */
+@SpringBootApplication
+public class LangChain4jDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangChain4jDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lulala/langchain4j/LangChain4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lulala.langchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangChain4jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
