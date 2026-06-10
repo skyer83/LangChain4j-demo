96c5d9347610e887fdbe588d23776d5ed0e9bf4a
shenjh <skyer_tech@163.com>
2026-06-10 14:19:58 +0800

    update 假装是另一个模型；

--- src/main/java/com/lulala/langchain4j/openai/service/GreetingExpert.java
+++ src/main/java/com/lulala/langchain4j/openai/service/GreetingExpert.java
@@ -7,11 +7,12 @@ import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
 /**
+ * 假装是另一个模型（用本地Ollama部署的qwen3.5:0.8b模型太慢了，也不够只能，就用别的模型代替）
  * @author shenjh
  * @version 1.0
  * @since 2026/6/9 17:43
  */
-@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, chatModel = LangChain4JConstants.ChatModel.OLLAMA_CHAT_MODEL)
+@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, chatModel = LangChain4JConstants.ChatModel.OPEN_AI_CHAT_MODEL)
 public interface GreetingExpert {
 
     @UserMessage("这是个问候语吗？文本：{{text}}")
