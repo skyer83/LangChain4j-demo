0b81ee8fab24f27b2930bc770ea17e16f655506c
shenjh <skyer_tech@163.com>
2026-07-15 17:52:06 +0800

    add 纯代理式 AI；

---
+++ src/main/java/com/lulala/langchain4j/agentic/controller/BankController.java
@@ -0,0 +1,15 @@
+package com.lulala.langchain4j.agentic.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 参见：https://langchain4j.cn/tutorials/agents.html<br/>
+ * 纯代理式 AI
+ * @author shenjh
+ * @version 1.0
+ * @since 2026/7/15 16:55
+ */
+@RestController
+public class BankController {
+
+}
